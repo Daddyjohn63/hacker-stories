bb4bcfe2de8c14b516a51d82accbbc9efeeb578a
Daddyjohn63 <john@webandprosper.co.uk>
2021-07-05 17:28:53 +0100

    upto controlled components page 57

--- src/App.js
+++ src/App.js
@@ -1,20 +1,71 @@
-import * as React from 'react';
+import * as React from "react"
 
-function getTitle(title) {
-  return title
-}
+const App = () => {
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org",
+      author: " Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org",
+      author: " Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1
+    }
+  ]
+  //lift state from Search to App
+  const [searchTerm, setSearchTerm] = React.useState("React")
 
-function App() {
+  //callback function
+  const handleSearch = event => {
+    setSearchTerm(event.target.value)
+    console.log(event.target.value)
+  }
+
+  const searchedStories = stories.filter(story => story.title.toLowerCase().includes(searchTerm.toLowerCase()))
+
   return (
     <div>
-      <h1>
-      Hello {getTitle('React')}
-      </h1>
+      <h1>My Hacker Stories</h1>
 
-      <label htmlFor="search">Search</label>
-      <input id="search" type="text" />
+      <Search search={searchTerm} onSearch={handleSearch} />
+      <hr />
+
+      <List list={searchedStories} />
     </div>
-  );
+  )
 }
 
-export default App;
+const Search = props => (
+  <div>
+    <label htmlFor="search">Search</label>
+    <input id="search" type="text" value={props.search} onChange={props.onSearch} />
+  </div>
+)
+
+const List = props => (
+  <ul>
+    {props.list.map(item => (
+      <Item key={item.objectID} item={item} />
+    ))}
+  </ul>
+)
+
+const Item = props => (
+  <li>
+    <span>
+      <a href={props.item.url}>{props.item.title}</a>
+    </span>
+    <span>{props.item.author}</span>
+    <span>{props.item.num_comments}</span>
+    <span>{props.item.points}</span>
+  </li>
+)
+
+export default App
